be634c492de088a73ffc08cf1d908e6ddd67b2a4
Essam <essamwisam@outlook.com>
2023-07-03 16:28:07 +0300

    ✨  Add LSTM from Scratch

--- botiverse/models/__init__.py
+++ botiverse/models/__init__.py
@@ -1,3 +1,4 @@
 from botiverse.models.FastSpeech1.FastSpeech import TTS
 from botiverse.models.NN.NN import NeuralNet
 from botiverse.models.SVM.SVM import SVM
+from botiverse.models.LSTM import LSTM
