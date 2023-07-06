240b4283ef448c9aac67e05b841acc52a2544813
Essam <essamwisam@outlook.com>
2023-07-06 23:22:28 +0300

    💥 Integrate Vocalizer

--- botiverse/preprocessors/__init__.py
+++ botiverse/preprocessors/__init__.py
@@ -1,5 +1,7 @@
 from botiverse.preprocessors.Vocalize.Vocalize import Vocalize
 from botiverse.preprocessors.Wav2Vec.Wav2Vec import Wav2Vec
+from botiverse.preprocessors.Wav2Vec.Wav2Vec import Wav2Text
+from botiverse.preprocessors.BertEmbeddings.BertEmbeddings import BertEmbedder
 from botiverse.preprocessors.GloVe.GloVe import GloVe
 from botiverse.preprocessors.TF_IDF.TF_IDF import TF_IDF
 from botiverse.preprocessors.TF_IDF_GLOVE.TF_IDF_GLOVE import TF_IDF_GLOVE
